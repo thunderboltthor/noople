513a5a0db22a2f33943d740c82f340f038cc1b08
Chris Herdt <cherdt@gmail.com>
2019-09-16 20:54:09 -0500

    Refactor search.py, update README

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # noople - the world's worst search engine
 
-(You can also call it DuckDuckNope if you like privacy.)
+(Change `SITE_NAME` to DuckDuckNope for enhanced privacy.)
 
 noople is a search engine that doesn't have any results. It is used for demonstration purposes only:
 
@@ -10,14 +10,37 @@ noople is a search engine that doesn't have any results. It is used for demonstr
 * Stored XSS vulnerabilities
 * SQL injection vulnerabilities
 
-## Reflected XSS
+## Run the application
+
+Create a Python virtual environment, if you haven't already:
+
+    python3 -m venv venv
+
+Activate your virtual environment:
+
+    source venv/bin/activate
+
+Install the requirements:
+
+    pip3 install -r requirements.txt
+
+Run the application:
+
+    export FLASK_APP=noople/search.py
+    flask run
+
+Visit http://localhost:5000 in a web browser.
+
+## (Known) Vulnerabilities
+
+### Reflected XSS
 
 To fix this, use `escape`
 
-## Stored XSS
+### Stored XSS
 
 To fix this, use `escape`
 
-## SQL injection
+### SQL injection
 
 To fix this, used bind variables
--- noople/search.py
+++ noople/search.py
@@ -1,23 +1,45 @@
+"""Noople: a trivial (and terrible) search engine
+
+Noople provides a search interface, but no search results.
+It is useful for demonstrating certain aspects of the
+Python Flask framework (routes, GET parameters) as well as
+common security vulnerabilities, such as XSS and SQLi.
+"""
+
 import sqlite3
-from flask import Flask, request
-from noople import db
+from flask import Flask, current_app, request
 
 app = Flask(__name__)
 
+# Change the following to DuckDuckNope for enhanced privacy
+SITE_NAME = 'noople'
+DB_PATH = 'instance/database.db'
 
-def insert_query(q=None):
+
+def check_db():
+    """Creates a database if none is present."""
+    try:
+        conn = sqlite3.connect(DB_PATH)
+        cursor = conn.cursor()
+        cursor.execute("SELECT id FROM query LIMIT 1;")
+        conn.close()
+    except:
+        init_db()
+
+
+def insert_query(search_query=None):
     """Insert a row into the query table
 
     Argument:
     q: the search text to be inserted
     """
-    if not q:
+    if not search_query:
         return
-    conn = sqlite3.connect('instance/database.db')
+    conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
 
     # Insert a row of data
-    cursor.execute("INSERT INTO query VALUES (NULL, '" + q + "');")
+    cursor.execute("INSERT INTO query VALUES (NULL, '" + search_query + "');")
 
     # Save (commit) the changes
     conn.commit()
@@ -32,14 +54,14 @@ def select_recent_queries(max_rows=5):
     Keyword argument:
     max_rows -- the number of rows to return
     """
-    conn = sqlite3.connect('instance/database.db')
+    conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
 
     # Insert a row of data
     cursor.execute("""SELECT search
                       FROM query
                       ORDER BY id DESC
-                      LIMIT """ + max_rows + ";")
+                      LIMIT """ + str(max_rows) + ";")
 
     results = cursor.fetchall()
 
@@ -49,43 +71,96 @@ def select_recent_queries(max_rows=5):
     return results
 
 
-@app.route('/')
-def search():
-    """Display search form and search results
+def get_search_results_html(search_query=None):
+    """Return search results, formatted as HTML"""
 
-    Optional GET parameter, q -- contains search string
-    """
-    # check for GET data
-    search_term = ''
-    recent_searches = ''
-    q = request.args.get("q", None)
-    if q:
-        search_term = '<p>You searched for: ' + q + '</p>'
-        search_term += '<p>No results found.</p>'
-        insert_query(q)
+    if search_query:
+        # Insert the search term into the database
+        insert_query(search_query)
 
+        # Format results as HTML
+        search_results = '<p>You searched for: ' + search_query + '</p>'
+        search_results += '<p>No results found.</p>'
+        return search_results
+
+    # No search term specified, so return an empty string
+    return ""
+
+
+def get_recent_searches_html():
+    """Return a formatted list of recent searches"""
+
+    # Get recent queries from the database
     recent_queries = select_recent_queries()
-    if len(recent_queries) > 1:
+
+    # If results were returned, create an HTML list
+    if recent_queries:
         recent_searches = '<p>Recent searches:</p><ul>'
         for query in recent_queries:
             recent_searches += '<li><a href="/?q=' + query[0] + '">'
             recent_searches += query[0]
             recent_searches += '</a></li>'
         recent_searches += '</ul>'
+        return recent_searches
+
+    # Otherwise, return a blank string
+    return ""
+
+
+@app.route('/')
+def search():
+    """Display search form and search results
+
+    Optional GET parameter, q -- contains search string
+    """
+    # Check for database tables
+    check_db()
+    # Check for GET data
+    search_query = request.args.get("q", None)
+    # Format search results as HTML
+    search_results = get_search_results_html(search_query)
+    # Format recent searches as HTML
+    recent_searches = get_recent_searches_html()
 
     return '''<h1>noople</h1>
               <form action="/" method="GET">
               <input type="text" name="q">
               <input type="submit" value="search">
-              </form>''' + search_term + recent_searches
+              </form>''' + search_results + recent_searches
 
 
 @app.route('/init/')
 def init():
+    """init route
+
+    Calls init and returns user to the main page.
+    """
+    try:
+        init_db()
+    except:
+        return '''<h1>An error occurred.</h1>
+                  <p>An error occurred while initializing
+                  the database. Try again?</p>'''
+
+    return '''<h1>Database initialized.</h1>
+              <p>Return to the <a href="/">main page</a>.</p>'''
+
+
+def init_db():
     """Initialize database
 
     Drops and recreates the query table
     """
-    db.init_db()
-    db.get_db().execute('SELECT * FROM query')
-    return 'Database initialized.'
+    # Open connection to the database
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+
+    # Open the schema file and execute its SQL code
+    with current_app.open_resource('schema.sql') as db_schema:
+        cursor.executescript(db_schema.read().decode('utf8'))
+
+    # Save (commit) the changes
+    conn.commit()
+
+    # We can also close the connection if we are done with it.
+    conn.close()
